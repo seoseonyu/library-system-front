45204b82c683f460731e435c885c71db68204471
koguempil <ko11380@visionerp.org>
2023-03-13 09:31:11 +0900

    User Side 페이지 변경 테스트 - 메뉴 키변경(임시)

--- src/main.tsx
+++ src/main.tsx
@@ -1,34 +1,43 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import {createBrowserRouter, Outlet, RouterProvider} from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 import ErrorPage from "./pages/error.page";
 import RootRoute from "./routes/root.route";
 import UserDashboard from "./pages/user/dashboard.page";
-import {ConfigProvider} from "antd";
+import { ConfigProvider } from "antd";
 import UserRoute from "./routes/user.route";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <RootRoute/>,
-    errorElement: <ErrorPage/>,
-  }, {
-    path: 'user',
-    element: <UserRoute/>,
+    element: <RootRoute />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "user",
+    element: <UserRoute />,
     children: [
       {
-        path: 'dashboard',
-        element: <UserDashboard/>
-      }
-    ]
-  }
-])
+        path: "dashboard",
+        element: <UserDashboard />,
+      },
+      {
+        path: "book",
+        element: <>book</>,
+      },
+      {
+        path: "manage",
+        element: <>manage</>,
+      },
+    ],
+  },
+]);
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <ConfigProvider direction='ltr'>
-      <RouterProvider router={router}/>
+    <ConfigProvider direction="ltr">
+      <RouterProvider router={router} />
     </ConfigProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
--- src/routes/user.route.tsx
+++ src/routes/user.route.tsx
@@ -1,29 +1,37 @@
-import {Button, Layout, Menu, MenuProps, Space} from "antd";
-import {Outlet} from "react-router-dom";
-import {Key, ReactNode, useState} from "react";
 import {
   BarcodeOutlined,
   BookOutlined,
   DashboardOutlined,
-  MenuFoldOutlined,
-  MenuUnfoldOutlined
 } from "@ant-design/icons";
+import { Layout, Menu, MenuProps, Space } from "antd";
+import { Key, ReactNode, useState } from "react";
+import { Outlet } from "react-router-dom";
 
-const {Header, Footer, Sider, Content} = Layout;
+const { Header, Footer, Sider, Content } = Layout;
 
-type MenuItem = Required<MenuProps>['items'][number];
+type MenuItem = Required<MenuProps>["items"][number];
 
-function getItem(label: ReactNode, key: Key, icon?: ReactNode, children?: MenuItem[], type?: 'group'): MenuItem {
+function getItem(
+  label: ReactNode,
+  key: Key,
+  icon?: ReactNode,
+  children?: MenuItem[],
+  type?: "group"
+): MenuItem {
   return {
-    key, icon, children, label, type
+    key,
+    icon,
+    children,
+    label,
+    type,
   } as MenuItem;
 }
 
 const items: MenuItem[] = [
-  getItem('대시보드', '1', <DashboardOutlined/>),
-  getItem('도서대출', '2', <BarcodeOutlined/>),
-  getItem('대출관리', '3', <BookOutlined/>),
-]
+  getItem("대시보드", "dashboard", <DashboardOutlined />),
+  getItem("도서대출", "book", <BarcodeOutlined />),
+  getItem("대출관리", "manage", <BookOutlined />),
+];
 
 const UserRoute = () => {
   const [collapsed, setCollapsed] = useState(false);
@@ -32,35 +40,46 @@ const UserRoute = () => {
     setCollapsed(!collapsed);
   };
 
+  const onClickMenuhandler: MenuProps["onClick"] = (info) => {
+    window.history.pushState({}, "", `${info.key}`);
+  };
+
   return (
-    <Space direction='vertical' style={{width: '100%'}} size={[0, 48]}>
+    <Space direction="vertical" style={{ width: "100%" }} size={[0, 48]}>
       <Layout hasSider>
         <Sider
-          style={{overflow: 'auto', height: '100vh', width:256}}
+          style={{ overflow: "auto", height: "100vh", width: 256 }}
           collapsible
           collapsed={collapsed}
           onClick={toggleCollapsed}
         >
-          <div style={{ height: 32, margin: 16, background: 'rgba(255, 255, 255, 0.2)' }} />
+          <div
+            style={{
+              height: 32,
+              margin: 16,
+              background: "rgba(255, 255, 255, 0.2)",
+            }}
+          />
           <Menu
-            theme='dark'
-            defaultSelectedKeys={['1']}
+            theme="dark"
+            defaultSelectedKeys={["dashboard"]}
             defaultOpenKeys={[]}
-            mode='inline'
+            mode="inline"
             items={items}
+            onClick={onClickMenuhandler}
           />
           <p>{collapsed}</p>
         </Sider>
         <Layout>
           <Header>Header</Header>
-          <Content style={{padding:10}}>
-            <Outlet/>
+          <Content style={{ padding: 10 }}>
+            <Outlet />
           </Content>
           <Footer>Footer</Footer>
         </Layout>
       </Layout>
     </Space>
   );
-}
+};
 
 export default UserRoute;
