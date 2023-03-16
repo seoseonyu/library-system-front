66b2a36fe89982484729c75a27c8e2e6eb69a808
koguempil <ko11380@visionerp.org>
2023-03-16 09:28:55 +0900

    Card.tsx 임시 커밋

---
+++ src/pages/components/dashboard/Card.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const Card = () => {
+  return <CardWarp></CardWarp>;
+};
+
+export default Card;
+
+const CardWarp = styled.div`
+  border: 1px solid #888888;
+`;
